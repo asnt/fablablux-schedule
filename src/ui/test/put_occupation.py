566d37069940d752b4e4fe7adcfb8cf9676da87f
Alexandre Saint <snt.alex@gmail.com>
2016-01-08 15:18:45 +0100

    Add command line parameters for host and port

--- src/ui/test/put_occupation.py
+++ src/ui/test/put_occupation.py
@@ -3,12 +3,13 @@
 import numpy as np
 import requests
 
+
 n_machines = 7
 n_slots = 9
 table = np.random.randint(2, size=(n_machines, n_slots)).astype(bool)
 
-def run():
-    address = "http://localhost:5000/api/occupation"
+def run(host, port):
+    address = "http://{:s}:{:d}/api/occupation".format(host, port)
     data = dict(table=table.tolist())
     r = requests.put(address, json=data)
     print(r.text)
@@ -16,4 +17,16 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    import sys
+    args = sys.argv[1:]
+    i = 0
+    host = "192.168.123.100"
+    port = 5000
+    while i < len(args):
+        if args[i] == '-h':
+            host = args[i + 1]
+            i += 1
+        elif args[i] == '-p':
+            port = int(args[i + 1])
+            i += 1
+    run(host=host, port=port)
