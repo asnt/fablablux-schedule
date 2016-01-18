0ce28ae136396fc80071e336a2210ac7148820c0
Alexandre Saint <snt.alex@gmail.com>
2016-01-18 12:06:18 +0100

    Refactor and clean up

--- src/scan.py
+++ src/scan.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 
 
-def rectify(image, reference, debug=False):
+def rectify(image, reference, detector='surf', n_features=5000, debug=False):
     """Transform an image to fit the reference image.
 
     Parameters
@@ -13,23 +13,40 @@ def rectify(image, reference, debug=False):
         Image to rectify.
     reference : (P, Q)
         Reference image.
-    transform: optional
-        The transform type to be used. ProjectiveTransform by default.
+    detector: string, optional
+        The feature detector to be used.
+    n_features: int, optional
+        The number of features to use.
     debug : bool, optional
         Displays some debug infos and saves intermediate results as images.
 
     Returns
     -------
     recified : (M', N')
-        Rectified image.
-    model : transform type 
-        Instance of the geometric transformation used.
+        The rectified image.
     """
-    detector = cv2.ORB(nfeatures=2000)
+    detector_name = detector
+    if detector == 'orb':
+        detector = cv2.ORB(nfeatures=n_features)
+        matcher = cv2.BFMatcher(normType=cv2.NORM_HAMMING, crossCheck=True)
+    elif detector == 'sift':
+        detector = cv2.SIFT(nfeatures=n_features)
+        matcher = cv2.BFMatcher(crossCheck=True)
+    elif detector == 'surf':
+        detector = cv2.SURF()
+        matcher = cv2.BFMatcher(crossCheck=True)
+    else:
+        raise ValueError('unknown detector: {:s}'.format(detector) +
+                         '(should be one of "orb", "sift", "surf"')
+
+    if debug:
+        print('detector = ' + detector_name)
+        if detector_name != 'surf':
+            print('n_features = ' + str(n_features))
+
     keypoints1, descriptors1 = detector.detectAndCompute(reference, None)
     keypoints2, descriptors2 = detector.detectAndCompute(image, None)
 
-    matcher = cv2.BFMatcher(normType=cv2.NORM_HAMMING, crossCheck=True)
     matches = matcher.match(descriptors1, descriptors2)
     keypoints1 = np.array([keypoints1[int(m.queryIdx)].pt for m in matches],
             dtype=np.float32)
@@ -37,10 +54,10 @@ def rectify(image, reference, debug=False):
             dtype=np.float32)
 
     transform_matrix, mask = cv2.findHomography(keypoints1, keypoints2,
-            cv2.RANSAC, 0.1)
+            cv2.RANSAC, 7)
     inliers = mask.ravel().tolist()
 
-    rectified = cv2.warpPerspective(image, transform_matrix, image.T.shape,
+    rectified = cv2.warpPerspective(image, transform_matrix, reference.T.shape,
             None, cv2.WARP_INVERSE_MAP)
 
     if debug:
@@ -75,6 +92,8 @@ def find_occupation(image, debug=False):
     -------
     occupation : (N_MACHINES, N_SLOTS), bool
         Occupation matrix (True if slot occupied).
+    output_image:
+        Image with detected slots highlighted.
     """
     n_machines = 7
     n_slots = 9
@@ -82,63 +101,70 @@ def find_occupation(image, debug=False):
 
     # Position of the top-left slot of the table
     r0, c0 = 140, 115
-    # Distance to subsequent rows (vertical direction)
+    # Distance between successive rows (vertical direction)
     d_rows = np.array([0, 62.5, 125, 220, 285, 375, 465])
     d_rows = d_rows / 2 # using half resolution image
-    # Distance to subsequent cols (horizontal direciton)
+    # Distance between successive cols (horizontal direction)
     d_cols = 78 * np.arange(n_slots)
     d_cols = d_cols / 2 # using half resolution image
     # Absolution position of the slots
     row_tile = np.tile(d_rows[:, np.newaxis], (1, n_slots))
     col_tile = np.tile(d_cols, (n_machines, 1))
-    pos = np.dstack((r0 + row_tile, c0 + col_tile))
+    slot_position = np.dstack((r0 + row_tile, c0 + col_tile))
 
+    image_float = image.astype(float)
+
+    radius = 15     # radius of a square patch encompassing a slot
 
     threshold = 2.5   # threshold at mean intensity level == 150
-    radius = 15     # radius of a square patch encompassing a slot
-    image_float = image.astype(float)
     # Adapt the threshold to the image mean intensity level.
     mean_intensity = image_float.mean()
     threshold = threshold * (mean_intensity / 150)
-    print(threshold)
-    # Measure of the slot roughness. Much roghness means the card is present,
-    # few roughness the card is not there.
+
+    if debug:
+        print("roughness threshold = {:.2f}".format(threshold))
+    
+    # Measure the slot roughness from which we deduce if a card is present.
     def roughness(patch):
         grad = np.gradient(patch)
         return np.mean(np.sqrt(grad[0]**2 + grad[1]**2))
-    # Helper function to check slot status.
+
+    # Small roughness level means card absent, i.e. slot booked.
+    # High roughness level means card present, i.e. not booked..
     def is_booked(patch):
         return roughness(patch) < threshold
-    for m in range(n_machines):
-        for s in range(n_slots):
-            r, c = pos[m, s]
-            patch = image_float[r-radius:r+radius, c-radius:c+radius]
-            # If the patch is smooth enough, it means there is no card, i.e.
-            # it is booked.
-            if roughness(patch) < threshold:
-                occupation[m, s] = True
 
     if debug:
-        slot_mask = np.ones_like(image, dtype=bool)
-        roughness_values = []
-        for m in range(n_machines):
-            for s in range(n_slots):
-                r, c = pos[m, s]
-                # save roughness value
-                patch = image_float[r-radius:r+radius, c-radius:c+radius]
-                roughness_values.append(roughness(patch))
-                # mask slots out
-                slot_mask[r-radius:r+radius, c-radius:c+radius] = False
-        image_slots = image.copy()
-        image_slots[slot_mask] = 0.5 * image_slots[slot_mask]
-        np.set_printoptions(precision=3)
-        print(np.reshape(np.array(roughness_values), (n_machines, n_slots)))
-        cv2.imwrite('debug/slots.png', image_slots)
+        slot_roughness = np.zeros((n_machines, n_slots))
 
-    return occupation
+    for machine_index in range(n_machines):
+        for slot_index in range(n_slots):
+            r, c = slot_position[machine_index, slot_index]
+            patch = image_float[r-radius:r+radius, c-radius:c+radius]
+            if is_booked(patch):
+                occupation[machine_index, slot_index] = True
+            if debug:
+                slot_roughness[machine_index, slot_index] = roughness(patch)
+
+    if debug:
+        np.set_printoptions(precision=2)
+        print(slot_roughness)
+
+    # Highlight the slots on the image for visualisation
+    slot_mask = np.ones_like(image, dtype=bool)
+    for m in range(n_machines):
+        for s in range(n_slots):
+            r, c = slot_position[m, s]
+            patch = image_float[r-radius:r+radius, c-radius:c+radius]
+            # create a mask for the slot
+            slot_mask[r-radius:r+radius, c-radius:c+radius] = False
+    output_image = image.copy()
+    output_image[slot_mask] = 0.5 * output_image[slot_mask]
+
+    return occupation, output_image
 
 
-def scan_table(image, banner, debug=False):
+def scan_table(image, banner, detector=None, n_features=None, debug=False):
     """Scan the image for the status of the slots in the occupation table.
 
     Parameters
@@ -147,6 +173,10 @@ def scan_table(image, banner, debug=False):
         Image to scan.
     banner : (M, N, 3)
         Reference image of the banner.
+    detector: string, optional
+        The feature detector used to rectify the image.
+    n_features: string, optional
+        The number of features for the detector.
     debug : bool, optional
         Displays some debug infos and saves intermediate results as images.
 
@@ -155,41 +185,113 @@ def scan_table(image, banner, debug=False):
     table : (N_MACHINES, N_SLOTS), bool
         Boolean matrix indicating the booking status of the machines.
     """
-    rectified = rectify(image, banner, debug)
+    kwargs = dict(debug=debug)
+    if detector is not None:
+        kwargs['detector'] = detector
+    if n_features is not None:
+        kwargs['n_features'] = n_features
+    rectified = rectify(image, banner, **kwargs)
 
     if debug:
-        cv2.imwrite('debug/banner.jpg', banner)
-        cv2.imwrite('debug/image.jpg', image)
-        cv2.imwrite('debug/rectified.jpg', rectified)
+        cv2.imwrite('debug/banner.png', banner)
+        cv2.imwrite('debug/image.png', image)
+        cv2.imwrite('debug/rectified.png', rectified)
 
-    occupation = find_occupation(rectified, debug)
+    occupation, output_image = find_occupation(rectified, debug=debug)
 
-    return occupation
+    return occupation, output_image
 
 
 def print_occupation(occupation):
+    print("<table>")
     for r in range(occupation.shape[0]):
         for c in range(occupation.shape[1]):
             print(occupation[r, c] and 'X' or '-', end=' ')
         print()
+    print("</table>")
 
 
-if __name__ == "__main__":
-    # usage example: python scanner.py [-d] image banner
+def parse_arguments():
+    """Parse the command line arguments.
+
+    Raise ValueError if the arguments are invalid or missing.
+    """
     import sys
     args = sys.argv[1:]
+
+    params = dict(debug=False,
+                  detector='surf',
+                  n_features=5000)
+
     if '-d' in args:
-        debug = True
+        params['debug'] = True
         del args[args.index('-d')]
-    else:
-        debug = False
-    image = cv2.imread(args[0], cv2.CV_LOAD_IMAGE_GRAYSCALE)
-    banner = cv2.imread(args[1], cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    if '-f' in args:
+        index = args.index('-f')
+        detector = args[index + 1]
+        if detector not in ['orb', 'sift', 'surf']:
+            raise ValueError('unknown detector: ' + detector + \
+                             "(must be one of 'orb', 'sift', 'surf'")
+        params['detector'] = detector
+        del args[index + 1]
+        del args[index]
+    if '-n' in args:
+        index = args.index('-n')
+        params['n_features'] = int(args[index + 1])
+        del args[index + 1]
+        del args[index]
+
+    if len(args) < 3:
+        raise ValueError("not enough arguments")
+    elif len(args) > 3:
+        raise ValueError("too many arguments")
+
+    params['input_file'] = args[0]
+    params['banner_file'] = args[1]
+    params['output_file'] = args[2]
+
+    return params
+
+
+usage_message = """\
+Usage: python scan.py [options] input_image banner output_image
+Options:
+    -d                  output debug messages and images
+    -f [surf|orb|sift]  feature detector, default surf
+    -n <integer>        number of features for orb and sift, default 5000
+"""
+
+def usage():
+    print(usage_message)
+
+
+def main():
+    try:
+        params = parse_arguments()
+    except ValueError as e:
+        print(e)
+        print()
+        usage()
+        import sys
+        sys.exit(1)
+
+    image = cv2.imread(params['input_file'], cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    banner = cv2.imread(params['banner_file'], cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
     import os.path
-    if debug and not os.path.exists('debug'):
+    if params['debug'] and not os.path.exists('debug'):
         import os
         os.mkdir('debug')
 
-    occupation = scan_table(image, banner, debug=debug)
+    detector = params['detector']
+    n_features = params['n_features']
+    debug = params['debug']
+    occupation, output_image = scan_table(image, banner, detector=detector,
+                                          n_features=n_features,
+                                          debug=debug)
     print_occupation(occupation)
+    cv2.imwrite(params['output_file'], output_image)
+
+
+if __name__ == "__main__":
+    main()
