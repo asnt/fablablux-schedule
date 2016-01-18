d52bcdebb9fed930faa9369965ed4404a19ee835
Alexandre Saint <snt.alex@gmail.com>
2016-01-18 15:17:53 +0100

    Refactor options related function into the MachineScheduleOptions class

--- src/wordpress-plugin/machine-schedule/machine-schedule.php
+++ src/wordpress-plugin/machine-schedule/machine-schedule.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: FabLab Luxembourg Machine Use Schedule
+Plugin Name: FabLabLux Machine Schedule
 Description: Add a live view of the machine use schedule to a page.
 Author: Alexandre Saint
 Version: 1.0
@@ -9,7 +9,7 @@ Version: 1.0
 defined('ABSPATH') or exit; 
 
 /**
- * Manage the options for the machine use schedule.
+ * Manage the options for the machine schedule.
  */
 class MachineScheduleOptions implements ArrayAccess {
 
@@ -17,11 +17,6 @@ class MachineScheduleOptions implements ArrayAccess {
      * Name of the option key.
      */
     const storage_name = "machine_use";
-
-    /**
-     * Unique id of the admin menu for managing the plugin options.
-     */
-    private $admin_menu_slug = self::prefix . '-admin_menu';
 
     /**
      * The default options array.
@@ -284,7 +279,7 @@ END;
         // Render the HTML form.
         $form = <<<END
 <div class="wrap">
-<h1>Machine Use Schedule Options</h1>
+<h1>Machine Schedule Options</h1>
 <form name="machine-use-schedule-options" method="post" action="">
 <input type="hidden" name="$field_action" value="update">
 
@@ -402,23 +397,11 @@ class Fablab_MachineSchedule {
     static $instance = false;
 
     /**
-     * Prefix of the metadata.
-     *
-     * $var string $prefix
-     */
-    const prefix = 'machine_schedule_';
-
-    /**
      * The key of the metadata used to store the table.
      *
      * $var string $meta_table
      */
-    private $meta_table = self::prefix . 'table';
-
-    /**
-     * Unique id of the admin menu for managing the plugin options.
-     */
-    private $admin_menu_slug = self::prefix . '-admin_menu';
+    private $meta_table = 'machine_schedule_table';
 
     /**
      * The options array.
@@ -486,247 +469,11 @@ class Fablab_MachineSchedule {
      * Add an options page in the admin menu.
      */
     public function admin_menu() {
-        /*
-        add_options_page('Machine Use Schedule Options',
-                         'Machine Use Schedule',
+        add_options_page('Machine Schedule Options',
+                         'Machine Schedule',
                          'manage_options',
-                         $this->admin_menu_slug,
-                         array($this, 'render_options'));
-         */
-        add_options_page('Machine Use Schedule Options',
-                         'Machine Use Schedule',
-                         'manage_options',
-                         $this->admin_menu_slug,
-                         array($this->options, 'render_options'));
-    }
-
-    /**
-     * Parse the textfield of opening hours.
-     *
-     * @return array
-     */
-    private function parse_opening_hours($string) {
-        $opening_hours = array();
-        $entries = explode("\n", $string);
-        foreach($entries as $entry) {
-            $tokens = explode(" ", $entry);
-            $day = $tokens[0];
-            $start = date_parse_from_format("H:i", $tokens[1]);
-            $end = date_parse_from_format("H:i", $tokens[2]);
-            $slot = array($day, $start['hour'], $start['minute'],
-                                $end['hour'], $end['minute']);
-            array_push($opening_hours, $slot);
-        }
-        return $opening_hours;
-    }
-
-    /**
-     * Format the opening hours array for editing in a textarea.
-     *
-     * @return string
-     */
-    private function format_opening_hours($opening_hours) {
-        $entries = array();
-        foreach($opening_hours as $entry) {
-            if (count($entry) != 5) {
-                $string = join(" ", $entry);
-            } else {
-                $string = sprintf("%s %02d:%02d %02d:%02d",
-                                  $entry[0], $entry[1], $entry[2],
-                                  $entry[3], $entry[4]);
-            }
-            array_push($entries, $string);
-        }
-        return join("\n", $entries);
-    }
-
-    /**
-     * Render the HTML of the options menu.
-     *
-     * @return string
-     */
-    public function render_options() {
-        if (!current_user_can('manage_options')) {
-            wp_die(('You do not have sufficient permissions to access this' .
-                    ' page.'));
-        }
-
-        // Define unique field names for the form inputs.
-        $field_action = 'machine_action';
-        $field_page_title = 'machine_page_title';
-        $field_machine_names = 'machine_machine_names';
-        $field_slot_names = 'machine_slot_names';
-        $field_opening_hours = 'machine_opening_hours';
-        $field_timezone = 'machine_timezone';
-
-        // Save changes if this is a POST request.
-        if (isset($_POST[$field_action])) {
-            $action = $_POST[$field_action];
-        } else {
-            $action = "";
-        }
-        if ($action == "update") {
-            $page_title = $_POST[$field_page_title];
-            $machine_names = $_POST[$field_machine_names];
-            $slot_names = $_POST[$field_slot_names];
-            $opening_hours = $_POST[$field_opening_hours];
-            $timezone = $_POST[$field_timezone];
-
-            $this->options['page_title'] = $page_title;
-            $this->options['machine_names'] = explode("\n", $machine_names);
-            $this->options['slot_names'] = explode("\n", $slot_names);
-            $hours_array = $this->parse_opening_hours($opening_hours);
-            $this->options['opening_hours'] = $hours_array;
-            $this->options['timezone'] = $timezone;
-
-            $this->options->save();
-
-            $message = "<div><p><strong>Changes saved!</strong></p></div>";
-            echo($message);
-        } else if ($action == "reset") {
-            $confirmation_form = <<<END
-<div>
-<p>
-You are about to reset the default options of the machine use schedule plugin.
-All your changes will be <strong>permanently</strong> deleted.
-</p>
-<p>
-Are you sure you want to proceed?
-</p>
-</div>
-<form name="machine-use-schedule-reset" method="post" action="">
-<input type="hidden" name="$field_action" value="confirm-reset">
-<input type="submit" name="button-confirm-reset" value="Yes, reset"
-       class="button button-primary">
-<input type="submit" name="button-cancel-reset" value="No, cancel"
-       class="button button-primary">
-</form>
-END;
-            echo($confirmation_form);
-            return;
-        } else if ($action == 'confirm-reset' &&
-                   isset($_POST['button-confirm-reset'])) {
-            $this->options->reset();
-            $this->options->save();
-            $message = "<div><p><strong>Options reset!</strong></p></div>";
-            echo($message);
-        }
-
-        // Retrieve option values.
-        $page_title = $this->options['page_title'];
-        $page_titles_html = "";
-        foreach(get_pages() as $page) {
-            $title = $page->post_title;
-            if ($title == $page_title) {
-                $page_titles_html .= "<option value='$title'" .
-                                     " selected='selected'>$title</option>";
-            } else {
-                $page_titles_html .= "<option value='$title'>$title</option>";
-            }
-        }
-        $machine_names = $this->options['machine_names'];
-        $machine_names_string = join("\n", $machine_names);
-        $slot_names = $this->options['slot_names'];
-        $slot_names_string = join("\n", $slot_names);
-        $hours = $this->options['opening_hours'];
-        $opening_hours_string = $this->format_opening_hours($hours);
-        $current_timezone = $this->options['timezone'];
-        $timezone_html_options = "";
-        foreach(DateTimeZone::listIdentifiers() as $timezone) {
-            if ($timezone == $current_timezone) {
-                $string = "<option name='$field_timezone' value='$timezone'"
-                          . " selected='selected'>$timezone</option>";
-            } else {
-                $string = "<option name='$field_timezone' value='$timezone'>"
-                          . "$timezone</option>";
-            }
-            $timezone_html_options .= $string;
-        }
-
-        // Render the HTML form.
-        $form = <<<END
-<div class="wrap">
-<h1>Machine Use Schedule Options</h1>
-<form name="machine-use-schedule-options" method="post" action="">
-<input type="hidden" name="$field_action" value="update">
-
-<table class="form-table">
-
-<tr>
-<th scope="row"><label for="$field_page_title">Target page:</label></th>
-<td>
-<select name="$field_page_title">
-    <option value='' default></option>
-    $page_titles_html
-</select>
-<p class="description">The page where to display the machine use schedule.</p>
-<p class="description">If blank, the schedule will not be displayed.</p>
-</td>
-</tr>
-
-<tr>
-<th scope="row"><label for="$field_machine_names">Machines names:</label></th>
-<td>
-<textarea name="$field_machine_names"
-          rows="7" cols="15">$machine_names_string</textarea>
-<p class="description">The header column of the table.</p>
-<p class="description">Put one machine per line.</p>
-</td>
-</tr>
-
-<tr>
-<th scope="row"><label for="$field_slot_names">Time slots names:</label></th>
-<td>
-<textarea name="$field_slot_names"
-          rows="9" cols="15">$slot_names_string</textarea>
-<p class="description">The header row of the table.</p>
-<p class="description">Put one slot per line.</p>
-</td>
-</tr>
-
-<tr>
-<th scope="row"><label for="$field_opening_hours">Opening hours:</label></th>
-<td>
-<textarea name="$field_opening_hours"
-          rows="5" cols="15">$opening_hours_string</textarea>
-<p class="description">
-Determine when to display the schedule.
-</p>
-<p class="description">
-Put one time slot per line.
-</p>
-<p class="description">
-The format is 'day start_time end_time'. For example, 'Thursday 14:00 21:00'.
-</p>
-<p class="description">
-The days are: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.
-</p>
-</td>
-</tr>
-
-<tr>
-<th scope="row"><label for="$field_timezone">Timezone:</label></th>
-<td>
-<select name="$field_timezone">
-    $timezone_html_options
-</select>
-</td>
-</tr>
-
-</table>
-
-<input type="submit" name="submit" value="Save changes"
-       class="button button-primary">
-</form>
-<br />
-<form name="machine-use-schedule" method="post" action="">
-<input type="hidden" name="$field_action" value="reset">
-<input type="submit" name="submit" value="Reset default options"
-       class="button button-secondary">
-</form>
-</div>
-END;
-        echo($form);
+                         'machine-schedule-admin-menu',
+                         array($this->options, 'render_admin_menu'));
     }
 
     /**
