641858d6c45d300829ba743a897e7070dafd1592
Alexandre Saint <snt.alex@gmail.com>
2016-01-25 14:08:26 +0100

    Move the rendering of the options form in its own class

---
+++ src/wordpress-plugin/machine-schedule/options-view.php
@@ -0,0 +1,414 @@
+<?php
+
+include_once(plugin_dir_path(__FILE__) . 'options.php');
+
+class OptionsView {
+
+    // Define unique names for the form inputs.
+    private static $input_names = array(
+        "action" => "machine_action",
+        "page_id" => "machine_page_id",
+        "machine_name" => "machine_machine_name",
+        "slot_name" => "machine_slot_name",
+        "machine_visible" => "machine_machine_visible",
+        "slot_visible" => "machine_slot_visible",
+        "opening_hours" => "machine_opening_hours",
+        "timezone" => "machine_timezone",
+    );
+
+    public static function render() {
+        $options = MachineScheduleOptions::instance();
+        $input_names = self::$input_names;
+
+        if (isset($_POST[$input_names['action']])) {
+            $action = $_POST[$input_names['action']];
+        } else {
+            $action = "";
+        }
+
+        // Save changes if this is a POST request.
+        if ($action == "update") {
+            $new_options = OptionsView::get_values();
+
+            $options['page_id'] = $new_options['page_id'];
+            $options['machine_names'] = $new_options['machine_names'];
+            $options['slot_names'] = $new_options['slot_names'];
+            $options['visible_machines'] = $new_options['visible_machines'];
+            $options['visible_slots'] = $new_options['visible_slots'];
+            $options['opening_hours'] = $new_options['opening_hours'];
+            $options['timezone'] = $new_options['timezone'];
+
+            $options->save();
+
+            $message = "<div><p><strong>Changes saved!</strong></p></div>";
+            echo($message);
+        } else if ($action == "reset") {
+            echo(self::render_confirmation());
+            return;
+        } else if ($action == 'confirm-reset' &&
+                   isset($_POST['button-confirm-reset'])) {
+            $options->reset();
+            $options->save();
+            $message = "<div><p><strong>Options reset!</strong></p></div>";
+            echo($message);
+        }
+
+        $selected_page_id = $options['page_id'];
+        $current_timezone = $options['timezone'];        
+        $opening_hours = $options['opening_hours'];        
+        $inputs = array(
+            "page" => self::render_page_selector($selected_page_id),
+            "machines" => self::render_machines(),
+            "timeslots" => self::render_timeslots(),
+            "opening_hours" => self::render_opening_hours($opening_hours),
+            "timezone" => self::render_timezone($current_timezone),
+        );
+        $form = self::render_form($inputs);
+
+        echo($form);
+    }
+
+    public static function render_confirmation() {
+        $input_names = self::$input_names;
+
+        $confirmation_form = <<<END
+<div>
+<p>
+You are about to reset the default options of the machine use schedule plugin.
+All your changes will be <strong>permanently</strong> deleted.
+</p>
+<p>
+Are you sure you want to proceed?
+</p>
+</div>
+<form name="machine-use-schedule-reset" method="post" action="">
+<input type="hidden" name="${input_names['action']}" value="confirm-reset">
+<input type="submit" name="button-confirm-reset" value="Yes, reset"
+       class="button button-primary">
+<input type="submit" name="button-cancel-reset" value="No, cancel"
+       class="button button-primary">
+</form>
+END;
+
+        return $confirmation_form;
+    }
+
+    private static function get_values() {
+        $options = MachineScheduleOptions::instance();
+        $input_names = self::$input_names;
+
+        $page_id = intval($_POST[$input_names['page_id']]);
+        $opening_hours = $_POST[$input_names['opening_hours']];
+        $timezone = $_POST[$input_names['timezone']];
+
+        $n_machines = count($options['machine_names']);
+
+        $machine_names = array();
+        for($index = 0; $index < $n_machines; $index++) {
+            $input_name = "${input_names['machine_name']}_${index}";
+            $name = $_POST[$input_name];
+            array_push($machine_names, $name); 
+        }
+
+        $visible_machines = array();
+        for($index = 0; $index < $n_machines; $index++) {
+            $input_name = "${input_names['machine_visible']}_${index}";
+            if (isset($_POST[$input_name])) {
+                $visible = true;
+            } else {
+                $visible = false;
+            }
+            array_push($visible_machines, $visible); 
+        }
+
+        $n_slots = count($options['slot_names']);
+
+        $slot_names = array();
+        for($index = 0; $index < $n_slots; $index++) {
+            $input_name = "${input_names['slot_name']}_${index}";
+            $name = $_POST[$input_name];
+            array_push($slot_names, $name); 
+        }
+
+        $visible_slots = array();
+        for($index = 0; $index < $n_slots; $index++) {
+            $input_name = "${input_names['slot_visible']}_${index}";
+            if (isset($_POST[$input_name])) {
+                $visible = true;
+            } else {
+                $visible = false;
+            }
+            array_push($visible_slots, $visible); 
+        }
+
+        $values = array();
+        $values['page_id'] = $page_id;
+        $values['machine_names'] = $machine_names;
+        $values['slot_names'] = $slot_names;
+        $values['visible_machines'] = $visible_machines;
+        $values['visible_slots'] = $visible_slots;
+        $hours_array = self::parse_opening_hours($opening_hours);
+        $values['opening_hours'] = $hours_array;
+        $values['timezone'] = $timezone;
+
+        return $values;
+    }
+
+    /**
+     * Render a select input with the list of pages.
+     *
+     * @return string
+     */
+    private static function render_page_selector($selected_page_id) {
+        $input_names = self::$input_names;
+        
+        $html = "<select name='${input_names['page_id']}'>";
+        $html .= "<option value='-1'></option>";
+
+        foreach(get_pages() as $page) {
+            $title = $page->post_title;
+            $id = $page->ID;
+            if ($id == $selected_page_id) {
+                $html .= "<option value='$id' selected='selected'>$title" .
+                         "</option>";
+            } else {
+                $html .= "<option value='$id'>$title</option>";
+            }
+        }
+
+        $html .= "</select>";
+
+        return $html;
+    }
+
+    /**
+     * Render inputs to modify the machine names and their visibility.
+     *
+     * @return string
+     */
+    private static function render_machines() {
+        $options = MachineScheduleOptions::instance();
+        $input_names = self::$input_names;
+
+        $machine_names = $options['machine_names'];
+        $visible_machines = $options['visible_machines'];
+
+        $html = "";
+        for($index = 0; $index < count($machine_names); $index++) {
+            $name = $machine_names[$index];
+            $visible = $visible_machines[$index] ? "checked='checked'" : "";
+
+            $html .= "<div>";
+            $html .= "<input type='text'" .
+                           " name='${input_names['machine_name']}_${index}'" .
+                           " value='$name'>";
+            $html .= "<input type='checkbox'" .
+                           " name='${input_names['machine_visible']}_${index}'" .
+                           " $visible>";
+            $html .= "</div>";
+        }
+
+        return $html;
+    }
+
+    /**
+     * Render inputs to modify the slot names and their visibility.
+     *
+     * @return string
+     */
+    private static function render_timeslots() {
+        $options = MachineScheduleOptions::instance();
+        $input_names = self::$input_names;
+
+        $slot_names = $options['slot_names'];
+        $visible_slots = $options['visible_slots'];
+
+        $html = "";
+        for($index = 0; $index < count($slot_names); $index++) {
+            $name = $slot_names[$index];
+            $visible = $visible_slots[$index] ? "checked='checked'" : "";
+            $html .= "<div>";
+            $html .= "<input type='text'" .
+                           " name='${input_names['slot_name']}_${index}'" .
+                           " value='$name'>";
+            $html .= "<input type='checkbox'" .
+                           " name='${input_names['slot_visible']}_${index}'" .
+                           " $visible>";
+            $html .= "</div>";
+        }
+
+        return $html;
+    }
+
+    private static function render_opening_hours($opening_hours) {
+        $input_names = self::$input_names;
+
+        $html = "<textarea name='${input_names['opening_hours']}'" .
+                         " rows='5' cols='15'>";
+        $html .= self::format_opening_hours($opening_hours);
+        $html .= "</textarea>";
+
+        return $html;
+    }
+
+    private static function render_timezone($current_timezone) {
+        $input_names = self::$input_names;
+
+        $html = "<select name='${input_names['timezone']}'>";
+        foreach(DateTimeZone::listIdentifiers() as $timezone) {
+            if ($timezone == $current_timezone) {
+                $html .= "<option value='$timezone'" .
+                                " selected='selected'>$timezone</option>";
+            } else {
+                $html .= "<option value='$timezone'>$timezone</option>";
+            }
+        }
+        $html .= "</select>";
+
+        return $html;
+    }
+
+    /**
+     * Render the options form.
+     *
+     * @param $inputs array() Array of the individual html inpus:
+     *                        $input = array(
+     *                          "page" => "...",
+     *                          "machines" => "...",
+     *                          "timeslots" => "...",
+     *                          "opening_hours" => "...",
+     *                          "timezone" = "...",
+     *                        )
+     * @return string
+     */
+    private static function render_form($inputs) {
+        $input_names = self::$input_names;
+
+        $html = <<<END
+<div class="wrap">
+<h1>Machine Schedule Options</h1>
+<form name="machine-use-schedule-options" method="post" action="">
+<input type="hidden" name="${input_names['action']}" value="update">
+
+<table class="form-table">
+
+<tr>
+<th scope="row">
+<label for="${input_names['page_id']}">Target page:</label>
+</th>
+<td>
+${inputs['page']}
+<p class="description">The page where to display the schedule.</p>
+<p class="description">If blank, the schedule is not displayed.</p>
+</td>
+</tr>
+
+<tr>
+<th scope="row"><label>Machines:</label></th>
+<td>
+${inputs['machines']}
+<p class="description">Unchecked machines are not displayed.</p>
+</td>
+</tr>
+
+<tr>
+<th scope="row"><label>Time slots:</label></th>
+<td>
+${inputs['timeslots']}
+<p class="description">If unchecked, the slot is not displayed.</p>
+</td>
+</tr>
+
+<tr>
+<th scope="row">
+<label for="${input_names['opening_hours']}">Opening hours:</label>
+</th>
+<td>
+${inputs['opening_hours']}
+<p class="description">
+Determine when to display the schedule.
+</p>
+<p class="description">
+Put one time slot per line.
+</p>
+<p class="description">
+The format is 'day start_time end_time'. For example, 'Thursday 14:00 21:00'.
+</p>
+<p class="description">
+The days are: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.
+</p>
+</td>
+</tr>
+
+<tr>
+<th scope="row">
+<label for="${input_names['timezone']}">Time zone:</label>
+</th>
+<td>
+${inputs['timezone']}
+<p class="description">
+Time zone of the opening hours.
+</p>
+</td>
+</tr>
+
+</table>
+
+<input type="submit" name="submit" value="Save changes"
+       class="button button-primary">
+</form>
+<br />
+<form name="machine-use-schedule" method="post" action="">
+<input type="hidden" name="${input_names['action']}" value="reset">
+<input type="submit" name="submit" value="Reset default options"
+       class="button button-secondary">
+</form>
+</div>
+END;
+
+        return $html;
+    }
+
+
+    /**
+     * Parse the textfield of opening hours.
+     *
+     * @return array
+     */
+    private static function parse_opening_hours($string) {
+        $opening_hours = array();
+        $entries = explode("\n", $string);
+        foreach($entries as $entry) {
+            $tokens = explode(" ", $entry);
+            $day = $tokens[0];
+            $start = date_parse_from_format("H:i", $tokens[1]);
+            $end = date_parse_from_format("H:i", $tokens[2]);
+            $slot = array($day, $start['hour'], $start['minute'],
+                                $end['hour'], $end['minute']);
+            array_push($opening_hours, $slot);
+        }
+        return $opening_hours;
+    }
+
+    /**
+     * Format the opening hours array for editing in a textarea.
+     *
+     * @return string
+     */
+    private static function format_opening_hours($opening_hours) {
+        $entries = array();
+        foreach($opening_hours as $entry) {
+            if (count($entry) != 5) {
+                $string = join(" ", $entry);
+            } else {
+                $string = sprintf("%s %02d:%02d %02d:%02d",
+                                  $entry[0], $entry[1], $entry[2],
+                                  $entry[3], $entry[4]);
+            }
+            array_push($entries, $string);
+        }
+        return join("\n", $entries);
+    }
+}
+
+?>
--- src/wordpress-plugin/machine-schedule/options.php
+++ src/wordpress-plugin/machine-schedule/options.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once(plugin_dir_path(__FILE__) . 'options-view.php');
+
 /**
  * Manage the options for the machine schedule.
  */
@@ -67,6 +69,16 @@ class MachineScheduleOptions implements ArrayAccess {
                          array($this, 'render_admin_menu'));
     }
 
+    public function render_admin_menu() {
+        if (!current_user_can('manage_options')) {
+            wp_die(('You do not have sufficient permissions to access this' .
+                    ' page.'));
+        }
+
+        OptionsView::render();
+    }
+
+
     /**
      * Load the options or create default ones if they do not yet exist.
      */
@@ -113,17 +125,6 @@ class MachineScheduleOptions implements ArrayAccess {
         $this->options = self::$default_options;
     }
 
-    /**
-     * Get the title of the currently defined page ID.
-     */
-    public function get_page_title() {
-        $page_id = $this['page_id'];
-        if (!is_page($page_id)) {
-            return "";
-        }
-        return get_the_title($page_id);
-    }
-
     //----------------------
     // ArrayAccess interface
     //----------------------
@@ -158,312 +159,6 @@ class MachineScheduleOptions implements ArrayAccess {
         } else {
             return null;
         }
-    }
-
-    //---------------------------------
-    // Rendering of options admin menu.
-    //---------------------------------
-
-    /**
-     * Parse the textfield of opening hours.
-     *
-     * @return array
-     */
-    private function parse_opening_hours($string) {
-        $opening_hours = array();
-        $entries = explode("\n", $string);
-        foreach($entries as $entry) {
-            $tokens = explode(" ", $entry);
-            $day = $tokens[0];
-            $start = date_parse_from_format("H:i", $tokens[1]);
-            $end = date_parse_from_format("H:i", $tokens[2]);
-            $slot = array($day, $start['hour'], $start['minute'],
-                                $end['hour'], $end['minute']);
-            array_push($opening_hours, $slot);
-        }
-        return $opening_hours;
-    }
-
-    /**
-     * Format the opening hours array for editing in a textarea.
-     *
-     * @return string
-     */
-    private function format_opening_hours($opening_hours) {
-        $entries = array();
-        foreach($opening_hours as $entry) {
-            if (count($entry) != 5) {
-                $string = join(" ", $entry);
-            } else {
-                $string = sprintf("%s %02d:%02d %02d:%02d",
-                                  $entry[0], $entry[1], $entry[2],
-                                  $entry[3], $entry[4]);
-            }
-            array_push($entries, $string);
-        }
-        return join("\n", $entries);
-    }
-
-    /**
-     * Render the HTML of the options menu.
-     */
-    public function render_admin_menu() {
-        if (!current_user_can('manage_options')) {
-            wp_die(('You do not have sufficient permissions to access this' .
-                    ' page.'));
-        }
-
-        // Define unique field names for the form inputs.
-        $field_action = 'machine_action';
-        $field_page_id = 'machine_page_id';
-        $field_machine_name = 'machine_machine_name';
-        $field_slot_name = 'machine_slot_name';
-        $field_machine_visible = 'machine_machine_visible';
-        $field_slot_visible = 'machine_slot_visible';
-        $field_opening_hours = 'machine_opening_hours';
-        $field_timezone = 'machine_timezone';
-
-        if (isset($_POST[$field_action])) {
-            $action = $_POST[$field_action];
-        } else {
-            $action = "";
-        }
-
-        // Save changes if this is a POST request.
-        if ($action == "update") {
-            $page_id = intval($_POST[$field_page_id]);
-            $opening_hours = $_POST[$field_opening_hours];
-            $timezone = $_POST[$field_timezone];
-
-            $n_machines = count($this['machine_names']);
-
-            $machine_names = array();
-            for($index = 0; $index < $n_machines; $index++) {
-                $field_name = "${field_machine_name}_${index}";
-                $name = $_POST[$field_name];
-                array_push($machine_names, $name); 
-            }
-
-            $visible_machines = array();
-            for($index = 0; $index < $n_machines; $index++) {
-                $field_name = "${field_machine_visible}_${index}";
-                if (isset($_POST[$field_name])) {
-                    $visible = true;
-                } else {
-                    $visible = false;
-                }
-                array_push($visible_machines, $visible); 
-            }
-
-            $n_slots = count($this['slot_names']);
-
-            $slot_names = array();
-            for($index = 0; $index < $n_slots; $index++) {
-                $field_name = "${field_slot_name}_${index}";
-                $name = $_POST[$field_name];
-                array_push($slot_names, $name); 
-            }
-
-            $visible_slots = array();
-            for($index = 0; $index < $n_slots; $index++) {
-                $field_name = "${field_slot_visible}_${index}";
-                if (isset($_POST[$field_name])) {
-                    $visible = true;
-                } else {
-                    $visible = false;
-                }
-                array_push($visible_slots, $visible); 
-            }
-
-            $this['page_id'] = $page_id;
-            $this ['machine_names'] = $machine_names;
-            $this ['slot_names'] = $slot_names;
-            $this ['visible_machines'] = $visible_machines;
-            $this ['visible_slots'] = $visible_slots;
-            $hours_array = $this->parse_opening_hours($opening_hours);
-            $this['opening_hours'] = $hours_array;
-            $this['timezone'] = $timezone;
-
-            $this->save();
-
-            $message = "<div><p><strong>Changes saved!</strong></p></div>";
-            echo($message);
-        } else if ($action == "reset") {
-            $confirmation_form = <<<END
-<div>
-<p>
-You are about to reset the default options of the machine use schedule plugin.
-All your changes will be <strong>permanently</strong> deleted.
-</p>
-<p>
-Are you sure you want to proceed?
-</p>
-</div>
-<form name="machine-use-schedule-reset" method="post" action="">
-<input type="hidden" name="$field_action" value="confirm-reset">
-<input type="submit" name="button-confirm-reset" value="Yes, reset"
-       class="button button-primary">
-<input type="submit" name="button-cancel-reset" value="No, cancel"
-       class="button button-primary">
-</form>
-END;
-            echo($confirmation_form);
-            return;
-        } else if ($action == 'confirm-reset' &&
-                   isset($_POST['button-confirm-reset'])) {
-            $this->reset();
-            $this->save();
-            $message = "<div><p><strong>Options reset!</strong></p></div>";
-            echo($message);
-        }
-
-        // Retrieve option values.
-
-        $page_id = $this['page_id'];
-        $page_titles_html = "";
-        foreach(get_pages() as $page) {
-            $title = $page->post_title;
-            $id = $page->ID;
-            if ($id == $page_id) {
-                $page_titles_html .= "<option value='$id'" .
-                                     " selected='selected'>$title</option>";
-            } else {
-                $page_titles_html .= "<option value='$id'>$title</option>";
-            }
-        }
-        $machine_names = $this['machine_names'];
-        $visible_machines = $this['visible_machines'];
-
-        $machine_html = "";
-        for($index = 0; $index < count($machine_names); $index++) {
-            $name = $machine_names[$index];
-            $visible = $visible_machines[$index] ? "checked='checked'" : "";
-
-            $machine_html .= "<div>";
-            $machine_html .= "<input type='text'" .
-                                   " name='${field_machine_name}_${index}'" .
-                                   " value='$name'>";
-            $machine_html .= "<input type='checkbox'" .
-                                   " name='${field_machine_visible}_${index}'" .
-                                   " $visible>";
-            $machine_html .= "</div>";
-        }
-        
-        $slot_names = $this['slot_names'];
-        $visible_slots = $this['visible_slots'];
-
-        $slot_html = "";
-        for($index = 0; $index < count($slot_names); $index++) {
-            $name = $slot_names[$index];
-            $visible = $visible_slots[$index] ? "checked='checked'" : "";
-            $slot_html .= "<div>";
-            $slot_html .= "<input type='text'" .
-                                 " name='${field_slot_name}_${index}'" .
-                                 " value='$name'>";
-            $slot_html .= "<input type='checkbox'" .
-                                 " name='${field_slot_visible}_${index}'" .
-                                 " $visible>";
-            $slot_html .= "</div>";
-        }
-
-        $hours = $this['opening_hours'];
-        $opening_hours_string = $this->format_opening_hours($hours);
-        $current_timezone = $this['timezone'];
-        $timezone_html_options = "";
-        foreach(DateTimeZone::listIdentifiers() as $timezone) {
-            if ($timezone == $current_timezone) {
-                $string = "<option name='$field_timezone' value='$timezone'"
-                          . " selected='selected'>$timezone</option>";
-            } else {
-                $string = "<option name='$field_timezone' value='$timezone'>"
-                          . "$timezone</option>";
-            }
-            $timezone_html_options .= $string;
-        }
-
-        // Render the HTML form.
-        $form = <<<END
-<div class="wrap">
-<h1>Machine Schedule Options</h1>
-<form name="machine-use-schedule-options" method="post" action="">
-<input type="hidden" name="$field_action" value="update">
-
-<table class="form-table">
-
-<tr>
-<th scope="row"><label for="$field_page_id">Target page:</label></th>
-<td>
-<select name="$field_page_id">
-    <option value='-1' default></option>
-    $page_titles_html
-</select>
-<p class="description">The page where to display the schedule.</p>
-<p class="description">If blank, the schedule is not displayed.</p>
-</td>
-</tr>
-
-<tr>
-<th scope="row"><label>Machines:</label></th>
-<td>
-$machine_html
-<p class="description">Unchecked machines are not displayed.</p>
-</td>
-</tr>
-
-<tr>
-<th scope="row"><label>Time slots:</label></th>
-<td>
-$slot_html
-<p class="description">If unchecked, the slot is not displayed.</p>
-</td>
-</tr>
-
-<tr>
-<th scope="row"><label for="$field_opening_hours">Opening hours:</label></th>
-<td>
-<textarea name="$field_opening_hours"
-          rows="5" cols="15">$opening_hours_string</textarea>
-<p class="description">
-Determine when to display the schedule.
-</p>
-<p class="description">
-Put one time slot per line.
-</p>
-<p class="description">
-The format is 'day start_time end_time'. For example, 'Thursday 14:00 21:00'.
-</p>
-<p class="description">
-The days are: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.
-</p>
-</td>
-</tr>
-
-<tr>
-<th scope="row"><label for="$field_timezone">Time zone:</label></th>
-<td>
-<select name="$field_timezone">
-    $timezone_html_options
-</select>
-<p class="description">
-Time zone of the opening hours.
-</p>
-</td>
-</tr>
-
-</table>
-
-<input type="submit" name="submit" value="Save changes"
-       class="button button-primary">
-</form>
-<br />
-<form name="machine-use-schedule" method="post" action="">
-<input type="hidden" name="$field_action" value="reset">
-<input type="submit" name="submit" value="Reset default options"
-       class="button button-secondary">
-</form>
-</div>
-END;
-        echo($form);
     }
 }
 
